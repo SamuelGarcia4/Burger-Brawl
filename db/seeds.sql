adc142777301e12b158428b21c0928ba78da9fbe
Samuel Garcia <sammyg413@gmail.com>
2018-07-14 11:56:47 -0600

    this thing

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pastrami Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegi Burger', FALSE);
